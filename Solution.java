1e5a6123fb34104afa70f9b2125e354e1809709f
jafar-sadiq-1 <22h51a0542@cmrcet.ac.in>
2024-11-18 10:42:24 +0530

    added scanner class

--- Solution.java
+++ Solution.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 class Solution{
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
         System.out.println("hellow world");
     }
 }
