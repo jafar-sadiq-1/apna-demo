af80a4c98bf8d945e750677f5aec0847c41705aa
jafar-sadiq-1 <22h51a0542@cmrcet.ac.in>
2024-11-18 10:44:08 +0530

    printing output

--- Solution.java
+++ Solution.java
@@ -4,6 +4,8 @@ class Solution{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
+
         System.out.println("hellow world");
+        System.out.println();
     }
 }
